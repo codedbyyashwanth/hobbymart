871047cbab91dd82be74fdee68c3c044311ecb64
Yashwanth <yashmy01@gmail.com>
2021-10-29 00:42:33 -0700

    Profile Page is Completed

--- app/src/main/java/com/laikapace/hobbymart/Home.java
+++ app/src/main/java/com/laikapace/hobbymart/Home.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
 
+    ImageView cartImage;
+    Animation cartImgAnime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        cartImage = findViewById(R.id.cart);
+        cartImgAnime = AnimationUtils.loadAnimation(this, R.anim.icon_anim);
     }
 
     public void Logout(View view) {
@@ -21,5 +31,17 @@ public class Home extends AppCompatActivity {
         auth.signOut();
         startActivity(new Intent(Home.this, Login.class));
         finish();
+    }
+
+    public void AddToCart(View view) {
+        cartImage.startAnimation(cartImgAnime);
+
+        Intent intent = new Intent(Home.this, Cart.class);
+        startActivity(intent);
+    }
+
+    public void Profile(View view) {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
